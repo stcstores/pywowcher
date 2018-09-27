42db40b6d91f5ffbf1cad8106f3ca9fca9c44974
Luke Shiner <luke@lukeshiner.com>
2018-09-27 11:24:58 +0100

    Docstring improvments

--- pywowcher/api_methods/status_method.py
+++ pywowcher/api_methods/status_method.py
@@ -7,19 +7,19 @@ class Status(BaseAPIMethod):
     """
     The Status API method.
 
-    Used to update the status of an order.
+    Used to update the status of one or more orders.
 
     Kwargs:
         orders: list containing dicts of order parameters in the form:
             {
-                “reference”: The Wowcher Code, e.g., 8UPGT3-KKQRNC,
-                “timestamp”: The Unix Timestamp of the update (optional, defaults to
+                "reference": The Wowcher Code, e.g., 8UPGT3-KKQRNC,
+                "timestamp": The Unix Timestamp of the update (optional, defaults to
                     the time of the request),
-                “status”: The updated status of the order; Status.RECIEVED_BY_MERCHANT,
+                "status": The updated status of the order; Status.RECIEVED_BY_MERCHANT,
                     Status.READY_FOR_DISPATCH or Status.DISPATCHED,
-                “tracking_number”: The courier supplied tracking number (optional),
-                “shipping_vendor”: The courier used to ship the order (optional),
-                “shipping_method”: The courier shipping method used to ship the order
+                "tracking_number": The courier supplied tracking number (optional),
+                "shipping_vendor": The courier used to ship the order (optional),
+                "shipping_method": The courier shipping method used to ship the order
                     (optional),
             }
     """
