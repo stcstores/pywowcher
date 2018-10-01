2091a806521b11c207f93a2a7a126ec923e76450
Luke Shiner <luke@lukeshiner.com>
2018-10-01 12:15:58 +0100

    Use pytest for tests
    Replace unitest with pytest for unit testing.

--- tests/test_api_methods.py
+++ tests/test_api_methods.py
@@ -1,162 +1,127 @@
 """Tests for Pywowcher API methods."""
 
 import datetime
-import json
-import os
 
-import pytest
 import pywowcher
 
-TEST_URI = "test_uri"
+from .basetests import BasePywowcherTest
 
 
-def get_test_api_method(uri, method):
-    """Return a new API method."""
-    API_method = pywowcher.api_methods.api_method.BaseAPIMethod
-    API_method.uri = uri
-    API_method.method = method
-    return API_method
+class TestBaseAPIMethod(BasePywowcherTest):
+    """Tests for the base APIMethod class."""
+
+    TEST_URI = "test_uri"
+
+    def get_test_api_method(self, uri, method):
+        """Return a new API method."""
+        API_method = pywowcher.api_methods.api_method.BaseAPIMethod
+        API_method.uri = uri
+        API_method.method = method
+        return API_method
+
+    def test_method_get_URL(self):
+        """Test the get_URL method of BaseAPIMethod."""
+        expected_URL = f"{pywowcher.WowcherAPISession.DOMAIN}{self.TEST_URI}"
+        API_method = self.get_test_api_method(
+            self.TEST_URI, pywowcher.api_methods.api_method.BaseAPIMethod.POST
+        )
+        assert expected_URL == API_method.get_URL()
+
+    def test_post_request(self, requests_mock):
+        """Test the WowcherAPISession.make_request method."""
+        response_text = "hello"
+        API_method = self.get_test_api_method(
+            self.TEST_URI, pywowcher.api_methods.api_method.BaseAPIMethod.POST
+        )
+        requests_mock.post(API_method.get_URL(), text=response_text)
+        response = API_method().call()
+        assert response.text == response_text
+
+    def test_get_request(self, requests_mock):
+        """Test the WowcherAPISession.make_request method."""
+        response_text = "hello"
+        API_method = self.get_test_api_method(
+            self.TEST_URI, pywowcher.api_methods.api_method.BaseAPIMethod.GET
+        )
+        requests_mock.register_uri("GET", API_method.get_URL(), text=response_text)
+        response = API_method().call()
+        assert response.text == response_text
 
 
-def test_method_get_URL():
-    """Test the get_URL method of BaseAPIMethod."""
-    expected_URL = f"{pywowcher.WowcherAPISession.DOMAIN}{TEST_URI}"
-    API_method = get_test_api_method(
-        TEST_URI, pywowcher.api_methods.api_method.BaseAPIMethod.POST
-    )
-    assert expected_URL == API_method.get_URL()
+class TestEchoTestAPIMethod(BasePywowcherTest):
+    """Tests for the Echo Test API method."""
+
+    def test_echo_test_method(self, mock_echo_test):
+        """Test the EchoTest API method."""
+        message = {"one": "1", "two": "2"}
+        mock_echo_test(message)
+        response = pywowcher.api_methods.EchoTest(message).call()
+        assert response == message
 
 
-def test_post_request(requests_mock):
-    """Test the WowcherAPISession.make_request method."""
-    response_text = "hello"
-    API_method = get_test_api_method(
-        TEST_URI, pywowcher.api_methods.api_method.BaseAPIMethod.POST
-    )
-    requests_mock.post(API_method.get_URL(), text=response_text)
-    response = API_method().call()
-    assert response.text == response_text
+class TestOrdersAPIMethod(BasePywowcherTest):
+    """Tests for the Orders API method."""
 
+    PAGE = 1
+    PER_PAGE = 100
+    FROM_DATE = datetime.datetime.now() - datetime.timedelta(days=1)
+    START_DATE = datetime.datetime.now() - datetime.timedelta(days=1)
+    END_DATE = datetime.datetime.now()
+    DEAL_ID = 9_856_321
 
-def test_get_request(requests_mock):
-    """Test the WowcherAPISession.make_request method."""
-    response_text = "hello"
-    API_method = get_test_api_method(
-        TEST_URI, pywowcher.api_methods.api_method.BaseAPIMethod.GET
-    )
-    requests_mock.register_uri("GET", API_method.get_URL(), text=response_text)
-    response = API_method().call()
-    assert response.text == response_text
-
-
-@pytest.fixture
-def orders_method_response():
-    """Return an example response for the Orders API method."""
-    with open(os.path.join(os.path.dirname(__file__), "order_response.json"), "r") as f:
-        return json.load(f)
-
-
-def mock_status(requests_mock):
-    """Set up a mocked status request."""
-    requests_mock.put(pywowcher.api_methods.Status.get_URL())
-
-
-def mock_echo_test(requests_mock, message=None, response=None):
-    """Set up a mocked echo test request."""
-    if message is not None:
-        response = {"json": echo_test_response(message)}
-    elif response is None:
-        raise Exception("Either kwargs or message must be passed")
-    if isinstance(response, dict):
-        response = [response]
-    requests_mock.post(pywowcher.api_methods.EchoTest.get_URL(), response)
-
-
-def mock_orders(requests_mock, response_data=None, response=None):
-    """Set up a mocked orders request."""
-    if response is None and response_data is None:
-        response = {"json": orders_method_response()}
-    elif response_data is not None:
-        response = {"json": response_data}
-    if isinstance(response, dict):
-        response = [response]
-    requests_mock.get(pywowcher.api_methods.Orders.get_URL(), response)
-
-
-def echo_test_response(message):
-    """Return the response from the Echo Test API method for a given message."""
-    return {"message": "Echo Test Received", "data": message}
-
-
-def test_echo_test_method(requests_mock):
-    """Test the EchoTest API method."""
-    message = {"one": "1", "two": "2"}
-    mock_echo_test(requests_mock, message)
-    response = pywowcher.api_methods.EchoTest(message).call()
-    assert response == message
-
-
-PAGE = 1
-PER_PAGE = 100
-FROM_DATE = datetime.datetime.now() - datetime.timedelta(days=1)
-START_DATE = datetime.datetime.now() - datetime.timedelta(days=1)
-END_DATE = datetime.datetime.now()
-DEAL_ID = 9856321
-
-
-def test_Orders_get_data_method(requests_mock):
-    """Test the get_data method of Orders."""
-    method = pywowcher.api_methods.Orders(
-        page=PAGE,
-        per_page=PER_PAGE,
-        from_date=FROM_DATE,
-        start_date=START_DATE,
-        end_date=END_DATE,
-        deal_id=DEAL_ID,
-    )
-    expected_data = {
-        "page": PAGE,
-        "per_page": PER_PAGE,
-        "from_date": FROM_DATE,
-        "start_date": START_DATE,
-        "end_date": END_DATE,
-        "deal_id": DEAL_ID,
-    }
-    assert method.data == expected_data
-
-
-def test_process_response_method(requests_mock, orders_method_response):
-    """Test the process_response method of Orders."""
-    requests_mock.get(
-        pywowcher.api_methods.Orders.get_URL(), json=orders_method_response
-    )
-    response = pywowcher.api_methods.Orders(
-        page=PAGE,
-        per_page=PER_PAGE,
-        from_date=FROM_DATE,
-        start_date=START_DATE,
-        end_date=END_DATE,
-        deal_id=DEAL_ID,
-    ).call()
-    assert response == orders_method_response
-
-
-def test_status_API_method(requests_mock):
-    """Test the Status API method."""
-    mock_status(requests_mock)
-    orders = [
-        {
-            "reference": "8UPGT3-KKQRNC",
-            "timestamp": 1234567890,
-            "status": 2,
-            "tracking_number": "JD1233230001012",
-            "shipping_vendor": "ROYAL_MAIL",
-            "shipping_method": "NEXT_DAY",
+    def test_Orders_get_data_method(self):
+        """Test the get_data method of Orders."""
+        method = pywowcher.api_methods.Orders(
+            page=self.PAGE,
+            per_page=self.PER_PAGE,
+            from_date=self.FROM_DATE,
+            start_date=self.START_DATE,
+            end_date=self.END_DATE,
+            deal_id=self.DEAL_ID,
+        )
+        expected_data = {
+            "page": self.PAGE,
+            "per_page": self.PER_PAGE,
+            "from_date": self.FROM_DATE,
+            "start_date": self.START_DATE,
+            "end_date": self.END_DATE,
+            "deal_id": self.DEAL_ID,
         }
-    ]
-    request = pywowcher.api_methods.Status(orders=orders)
-    sent_data = request.get_data(orders=orders)
-    assert pywowcher.api_methods.Status.ORDERS in sent_data
-    assert sent_data[request.ORDERS][0]["reference"] == orders[0]["reference"]
-    response = request.call()
-    assert response.status_code == 200
+        assert method.data == expected_data
+
+    def test_process_orders_response_method(self, mock_orders, orders_method_response):
+        """Test the process_response method of Orders."""
+        mock_orders()
+        response = pywowcher.api_methods.Orders(
+            page=self.PAGE,
+            per_page=self.PER_PAGE,
+            from_date=self.FROM_DATE,
+            start_date=self.START_DATE,
+            end_date=self.END_DATE,
+            deal_id=self.DEAL_ID,
+        ).call()
+        assert response == orders_method_response
+
+
+class TestStatusAPIMethod(BasePywowcherTest):
+    """Tests for the Status API method."""
+
+    def test_status_API_method(self, mock_status):
+        """Test the Status API method."""
+        mock_status()
+        orders = [
+            {
+                "reference": "8UPGT3-KKQRNC",
+                "timestamp": 1_234_567_890,
+                "status": 2,
+                "tracking_number": "JD1233230001012",
+                "shipping_vendor": "ROYAL_MAIL",
+                "shipping_method": "NEXT_DAY",
+            }
+        ]
+        request = pywowcher.api_methods.Status(orders=orders)
+        sent_data = request.get_data(orders=orders)
+        assert pywowcher.api_methods.Status.ORDERS in sent_data
+        assert sent_data[request.ORDERS][0]["reference"] == orders[0]["reference"]
+        response = request.call()
+        assert response.status_code == 200
--- tests/test_session.py
+++ tests/test_session.py
@@ -8,70 +8,53 @@ import yaml
 import pytest
 import pywowcher
 
-fake_key = "0ff52fd6-7860-4f07-bab5-5fa74d3b98f0"
-fake_secret_token = "16459c82-065a-4e51-b682-c784e404831d"
+from .basetests import BasePywowcherTest
 
 
-@pytest.fixture
-def temporary_cwd(tmpdir):
-    """Use a temporary directory as the working directory."""
-    os.chdir(tmpdir.mkdir("temp_working_directory"))
+class TestSession(BasePywowcherTest):
+    """Tests for pywowcher API sessions."""
 
-    def create_temporary_working_directory(config_file=True):
-        if config_file is True:
-            pywowcher.WowcherAPISession.create_credentials_file(
-                key=fake_key, secret_token=fake_secret_token
-            )
-            pywowcher.WowcherAPISession.key = None
-            pywowcher.WowcherAPISession.secret_token = None
+    @pytest.fixture
+    def no_config_file(self):
+        """Remove the wowcher credentials file from the working directory."""
+        os.remove(pywowcher.WowcherAPISession.WOWCHER_CREDENTIALS_FILENAME)
 
-    return create_temporary_working_directory
+    def test_get_credentials_fails_when_no_file_is_present(self, no_config_file):
+        """Test that getting credentials throws and error when no credentials file exists."""
+        with pytest.raises(FileNotFoundError):
+            pywowcher.WowcherAPISession.get_credentials()
 
+    def test_exception_is_raised_when_config_file_is_malformed(self, no_config_file):
+        """Test that an exception is raised when the config file is malformed."""
+        filename = pywowcher.WowcherAPISession.WOWCHER_CREDENTIALS_FILENAME
+        with open(filename, "w") as f:
+            f.write("some text")
+        with pytest.raises(Exception):
+            pywowcher.WowcherAPISession.get_credentials()
+        os.remove(filename)
 
-def test_get_credentials_fails_when_no_file_is_present(temporary_cwd):
-    """Test that getting credentials throws and error when no credentials file exists."""
-    temporary_cwd(config_file=False)
-    pywowcher.WowcherAPISession.key = None
-    pywowcher.WowcherAPISession.secret_token = None
-    with pytest.raises(FileNotFoundError):
+    def test_create_credentials_file(self):
+        """Test that the WowcherAPISession class can create a credentials file."""
+        pywowcher.WowcherAPISession.create_credentials_file(
+            key=self.fake_key, secret_token=self.fake_secret_token
+        )
+        filename = pywowcher.WowcherAPISession.WOWCHER_CREDENTIALS_FILENAME
+        assert os.path.exists(filename)
+        with open(filename, "r") as config_file:
+            config = yaml.load(config_file)
+        assert config == {"key": self.fake_key, "secret_token": self.fake_secret_token}
+        os.remove(filename)
+
+    def test_can_find_credentials_file(self):
+        """Test that Pywowcher can find the credentials file."""
+        found_path = pywowcher.WowcherAPISession.get_wowcher_credentials_file()
+        expected_path = os.path.join(
+            os.getcwd(), pywowcher.WowcherAPISession.WOWCHER_CREDENTIALS_FILENAME
+        )
+        assert found_path == expected_path
+
+    def test_session_can_load_credentials(self):
+        """Test that WowcherAPISession can load credentials from a file."""
         pywowcher.WowcherAPISession.get_credentials()
-
-
-def test_exception_is_raised_when_config_file_is_malformed(temporary_cwd):
-    """Test that an exception is raised when the config file is malformed."""
-    temporary_cwd(config_file=False)
-    with open(pywowcher.WowcherAPISession.WOWCHER_CREDENTIALS_FILENAME, "w") as f:
-        f.write("some text")
-    with pytest.raises(Exception):
-        pywowcher.WowcherAPISession.get_credentials()
-
-
-def test_create_credentials_file(temporary_cwd):
-    """Test that the WowcherAPISession class can create a credentials file."""
-    temporary_cwd(config_file=False)
-    pywowcher.WowcherAPISession.create_credentials_file(
-        key=fake_key, secret_token=fake_secret_token
-    )
-    filename = pywowcher.WowcherAPISession.WOWCHER_CREDENTIALS_FILENAME
-    assert os.path.exists(filename)
-    with open(filename, "r") as config_file:
-        config = yaml.load(config_file)
-    assert config == {"key": fake_key, "secret_token": fake_secret_token}
-
-
-def test_can_find_credentials_file(temporary_cwd):
-    """Test that Pywowcher can find the credentials file."""
-    temporary_cwd(config_file=True)
-    found_path = pywowcher.WowcherAPISession.get_wowcher_credentials_file()
-    expected_path = os.path.join(
-        os.getcwd(), pywowcher.WowcherAPISession.WOWCHER_CREDENTIALS_FILENAME
-    )
-    assert found_path == expected_path
-
-
-def test_session_can_load_credentials(temporary_cwd):
-    """Test that WowcherAPISession can load credentials from a file."""
-    temporary_cwd(config_file=True)
-    pywowcher.WowcherAPISession.get_credentials()
-    assert pywowcher.WowcherAPISession.key == fake_key
-    assert pywowcher.WowcherAPISession.secret_token == fake_secret_token
+        assert pywowcher.WowcherAPISession.key == self.fake_key
+        assert pywowcher.WowcherAPISession.secret_token == self.fake_secret_token
