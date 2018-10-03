836756de02f3a6af08ff0e6946b1fb83ca2376b5
Luke Shiner <luke@lukeshiner.com>
2018-10-03 11:04:07 +0100

    refactor: Use instance for session
    Use a class instance for holding the session credentials instead of a
    class.

--- pywowcher/wowcher_session.py
+++ pywowcher/wowcher_session.py
@@ -17,69 +17,71 @@ class WowcherAPISession:
     key = None
     secret_token = None
 
-    @classmethod
-    def get_auth_headers(cls):
+    def get_auth_headers(self):
         """Return authorisation headers."""
-        auth_string = f"{cls.key}:{cls.secret_token}"
+        auth_string = f"{self.key}:{self.secret_token}"
         encoded_auth_string = base64.b64encode(auth_string.encode("utf-8"))
         return {"Authorization": encoded_auth_string}
 
-    @classmethod
-    def get_wowcher_credentials_file(cls, directory=None):
+    def get_wowcher_credentials_file(self, directory=None):
         """Return the path to the wowcher credentials file."""
         if directory is None:
             directory = os.getcwd()
-        if os.path.exists(os.path.join(directory, cls.WOWCHER_CREDENTIALS_FILENAME)):
-            return os.path.join(directory, cls.WOWCHER_CREDENTIALS_FILENAME)
+        if os.path.exists(os.path.join(directory, self.WOWCHER_CREDENTIALS_FILENAME)):
+            return os.path.join(directory, self.WOWCHER_CREDENTIALS_FILENAME)
         elif os.path.dirname(directory) == directory:
             return None
         else:
-            return cls.get_wowcher_credentials_file(
+            return self.get_wowcher_credentials_file(
                 directory=os.path.dirname(directory)
             )
 
-    @classmethod
-    def get_credentials(cls):
+    def get_credentials(self):
         """Find API credentials file and load credentials from it."""
-        if cls.key and cls.secret_token is not None:
+        if self.key is not None and self.secret_token is not None:
             return
-        credentials_path = cls.get_wowcher_credentials_file()
+        credentials_path = self.get_wowcher_credentials_file()
         if credentials_path is None:
             logging.error("Wowcher credentials file not found.")
             raise FileNotFoundError(
-                f"{cls.WOWCHER_CREDENTIALS_FILENAME} was not found."
+                f"{self.WOWCHER_CREDENTIALS_FILENAME} was not found."
             )
         else:
             logger.debug(f"Credentials file found at {credentials_path}")
-            cls.load_credentials_from_file(credentials_path)
+            self.load_credentials_from_file(credentials_path)
 
-    @classmethod
-    def load_credentials_from_file(cls, credentials_path):
+    def load_credentials_from_file(self, credentials_path):
         """Add API credentials from file."""
         logger.info(f"Loading API Credentials from {credentials_path}")
         with open(credentials_path, "r") as config_file:
             config = yaml.load(config_file)
         try:
-            cls.set_credentials(**config)
+            self.set_credentials(**config)
         except Exception as e:
             logger.error(f"Could not load config from {credentials_path}.")
             raise e
 
-    @classmethod
-    def set_credentials(cls, key=None, secret_token=None):
+    def set_credentials(self, key=None, secret_token=None):
         """Set the domain, username and password."""
         if key is not None:
-            cls.key = key
+            self.key = key
         if secret_token is not None:
-            cls.secret_token = secret_token
+            self.secret_token = secret_token
 
-    @classmethod
-    def create_credentials_file(cls, *, key, secret_token):
+    def create_credentials_file(self, *, key, secret_token):
         """Create an API credential file."""
-        path = os.path.join(os.getcwd(), cls.WOWCHER_CREDENTIALS_FILENAME)
+        path = os.path.join(os.getcwd(), self.WOWCHER_CREDENTIALS_FILENAME)
         with open(path, "w") as outfile:
             yaml.dump(
                 {"key": key, "secret_token": secret_token},
                 outfile,
                 default_flow_style=False,
             )
+
+    def clear(self):
+        """Clear the session credentials."""
+        self.key = None
+        self.secret_token = None
+
+
+session = WowcherAPISession()
