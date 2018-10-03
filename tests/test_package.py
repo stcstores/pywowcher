dc5dd1fa547a2eb0d9479884228e1741a977578a
Luke Shiner <luke@lukeshiner.com>
2018-10-03 13:55:00 +0100

    test: Add tests for __revision__

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,16 +1,17 @@
 """Tests to assert that pywowcher functions correctly as a package."""
 
 
-from pywowcher import __version__
+import pywowcher
 
 
 def test_version_info():
     """Check the __version__ file has the neccessary attributes."""
-    assert hasattr(__version__, "__title__")
-    assert hasattr(__version__, "__description__")
-    assert hasattr(__version__, "__url__")
-    assert hasattr(__version__, "__version__")
-    assert hasattr(__version__, "__author__")
-    assert hasattr(__version__, "__author_email__")
-    assert hasattr(__version__, "__license__")
-    assert hasattr(__version__, "__copyright__")
+    assert hasattr(pywowcher, "__title__")
+    assert hasattr(pywowcher, "__description__")
+    assert hasattr(pywowcher, "__url__")
+    assert hasattr(pywowcher, "__version__")
+    assert hasattr(pywowcher, "__release__")
+    assert hasattr(pywowcher, "__author__")
+    assert hasattr(pywowcher, "__author_email__")
+    assert hasattr(pywowcher, "__license__")
+    assert hasattr(pywowcher, "__copyright__")
